51e3016e1c5a8af92bc3c7cfa24688d8de55e7ca
gamesketches <sam.von.ehren@gmail.com>
2020-09-23 12:27:11 -0400

    commiting first prototype

---
+++ Assets/PhoneCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneCam : MonoBehaviour
+{
+
+    public Material renderMat;
+    WebCamTexture texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        texture = new WebCamTexture();
+        foreach (WebCamDevice device in devices)
+        {
+            Debug.Log(device.name);
+            texture.deviceName = "FaceTime HD Camera (Built-in)";
+            texture.Play();
+        }
+        renderMat.mainTexture = texture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           if(Input.GetKeyDown(KeyCode.Space))
+        {
+            texture.Play();
+
+        }
+    }
+}
---
+++ Assets/cameraBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehavior : MonoBehaviour
+{
+    public Vector3 pivot;
+    public float rotationSpeed = 3;
+    public Vector3 winSpot;
+    public GameObject xwd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.RotateAround(pivot, Vector3.up, rotationSpeed * Time.deltaTime);
+        } else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.RotateAround(pivot, Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = Vector3.Lerp(transform.position, pivot, 0.01f);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = Vector3.LerpUnclamped(transform.position, pivot, -0.01f);
+        }
+        if(Vector3.Distance(transform.position, winSpot) < 2)
+        {
+            Debug.Break();
+            xwd.SetActive(true);
+            foreach(GameObject obj in GameObject.FindGameObjectsWithTag(("letters")))
+            {
+                obj.SetActive(false);
+            }
+
+        }
+    }
+}
